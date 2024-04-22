c8a45c00bf989e6121a96619d087b2cf84286c44
Antonykiroles601 <antony_esk@hotmail.com>
2024-04-22 22:19:56 +0200

    It works but desynced cause of ffplay

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
+import dis
 import threading
+import uvloop
 import websockets
 import asyncio
 import json    
@@ -6,44 +8,16 @@ from queue import Queue, Empty
 import numpy as np
 import cv2
 import time
+import io
 
-async def captureRadio(url:str):
-    # ffmpeg = [
-    #     "ffmpeg",
-    #     '-nostdin',
-    #     '-v',
-    #     'debug',
-    #     "-i",
-    #     url,
-    #     "-f",
-    #     's16le',
-    #     "-acodec",
-    #     "pcm_s16le",
-    #     "-ar", 
-    #     "16000",
-    #     "-ac",
-    #     "1",
-    #     'pipe:1',
-    # ]
-    print(" ".join(ffmpeg))
-    try:
-        process = await asyncio.create_subprocess_exec(*ffmpeg, stdout=asyncio.subprocess.PIPE)
-        # process= await asyncio.create_subprocess_exec(*("ffmpeg -i https://radio.talksport.com/stream -f s16le -acodec pcm_s16le -ar 16000 -ac 1 pipe:1".split(" ")), stdout=asyncio.subprocess.PIPE)
 
-    except Exception as e:
-        print("FFMPEG ERROR", e)
-        raise e
-    while True:
-        data = await process.stdout.read(1024)
 
-        yield data
-        
 async def recv(frames : Queue, audio: asyncio.Queue, websocket: websockets.WebSocketClientProtocol):
     try:
         while True:
-            print("RECEIVING")
+            # print("RECEIVING")
             frame = await websocket.recv()
-            print(len(frame))
+            # print(len(frame))
             if frame == "DONE":
                 print("DONE")
                 frames.put(False)
@@ -55,7 +29,7 @@ async def recv(frames : Queue, audio: asyncio.Queue, websocket: websockets.WebSo
                 continue
             # frame = cv2.imdecode(np.frombuffer(frame, dtype=np.uint8), flags=1)
             if frame is not None and isinstance(frame, bytes) and len(frame)>0:
-                print("RECEIVED", len(frame))
+                # print("RECEIVED", len(frame))
                 try:
                     f = frame[9:9+int.from_bytes(frame[5:9], 'little')]
                     f = np.frombuffer(f[12:], dtype=np.uint8)
@@ -66,7 +40,7 @@ async def recv(frames : Queue, audio: asyncio.Queue, websocket: websockets.WebSo
                     c = time.time()
                     s = c
                     # first = False
-                    print("received frame")
+                    # print("received frame")
                 except Exception as e:
                     print(e)
     except websockets.exceptions.ConnectionClosed as e:
@@ -77,46 +51,36 @@ async def recv(frames : Queue, audio: asyncio.Queue, websocket: websockets.WebSo
         pass
     frames.put(False)
     
-async def playAudio(pcm : asyncio.Queue[bytes]):
+async def playAudio(pcm : asyncio.Queue[bytes],frames: Queue):
     # with open("test.pcm", "wb") as f:
     try:
-        i = 0
-        pcmRemenants = b''
         while pcm.qsize()<17:
             # print("audioWaiting",pcm.qsize())
-            time.sleep(1/30)
+            await asyncio.sleep(1/30)
         ffplay = await asyncio.create_subprocess_exec("ffplay","-f","s16le","-ar","16000","-ac","1",'-acodec','pcm_s16le',"pipe:0",stdin=asyncio.subprocess.PIPE)
+        # while pcm.qsize()>0:
+        #     await ffplay.stdin.write(await pcm.get())
+        #     await ffplay.stdin.drain()
+        displayTask = threading.Thread(target = Display,args=(frames,))
+        displayTask.start()
+
         while True:
             s = time.time()
             pcmBytes = await pcm.get()    
             if pcmBytes is None or not pcmBytes:
                 print("END AUDIO")
                 break
-            # print(len(pcmBytes))
-            # i = 0
-            # for i in range(0,len(pcmBytes),320):
-            #     vmic.write_frames(pcmBytes[i:i+320])
-            # if i < len(pcmBytes):
-            pcmBytes = pcmRemenants + pcmBytes
-            cutOff = len(pcmBytes)//320*320
+
             if ffplay.stdin is not None:
-                ffplay.stdin.write(pcmBytes[:cutOff])
+                ffplay.stdin.write(pcmBytes)
                 await ffplay.stdin.drain()
-            # f.write(pcmBytes[:cutOff])
-            pcmRemenants = pcmBytes[cutOff:]
-            print(len(pcmBytes), len(pcmRemenants))
-            # sleepTime= 1/30 - (time.time()-s)
-            # print(sleepTime)
-            # if sleepTime > 0:    
-            #     time.sleep(sleepTime)
-            
-            # vmic.write_frames(pcmBytes[i+320:])
     except Exception as e:
         print("PLAY AUDIO ERROR", e)
         pass
+    finally:
+        displayTask.join()
 
-def Display(frames : asyncio.Queue):
-    pcmQueue = Queue()
+def Display(frames : Queue):
     try:
         i = 0
         namedWindow = "Video"
@@ -124,6 +88,7 @@ def Display(frames : asyncio.Queue):
         while frames.qsize()<17:
             cv2.imshow(namedWindow,np.zeros((512,512,3),dtype=np.uint8))
             time.sleep(1/30)
+            print("waiting for frames")
         while True:
             s = time.time()
             try:
@@ -135,9 +100,10 @@ def Display(frames : asyncio.Queue):
                 print("END")
                 break
             elif isinstance(frame, np.ndarray):
-                frame  =cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                cv2.imshow(frame) # type: ignore
+                # frame  =cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                cv2.imshow(namedWindow,frame) # type: ignore
                 c = time.time()
+                cv2.waitKey(1)
                 sleepTime = 1/30 - c + s
                 if sleepTime > 1/30:
                     print(sleepTime)
@@ -148,13 +114,19 @@ def Display(frames : asyncio.Queue):
         print("DISPLAY ERROR", e)
         pass
     finally:
-        pcmQueue.put(None)
         print("DISPLAY DONE")
 
-async def send(websocket:websockets.WebSocketClientProtocol,data:str):
-    async for d in captureRadio(data):
-        await websocket.send(d)
-        print(len(d))
+async def send(websocket:websockets.WebSocketClientProtocol,process: asyncio.subprocess.Process):
+    print("SENDING")
+    while True:
+        if process.stdout is None:
+            print("NO STDOUT")
+            break
+        data = await process.stdout.read(4096)
+        # print(len(data))
+        await websocket.send(data)
+    process.kill()
+    await process.wait()
 
 async def echo():   
     async with websockets.connect("ws://34.91.9.107:8892/LipsyncStream") as websocket:
@@ -166,15 +138,44 @@ async def echo():
             'syncAudio': True
         }
         await websocket.send(json.dumps(metadata))
-        sendTask = asyncio.create_task(send(websocket,"https://radio.talksport.com/stream"))
+        url = "https://radio.talksport.com/stream"
+        ffmpeg = [
+            "ffmpeg",
+            '-nostdin',
+            '-v',
+            'error',
+            "-i",
+            url,
+            "-f",
+            's16le',
+            "-acodec",
+            "pcm_s16le",
+            "-ar", 
+            "16000",
+            "-ac",
+            "1",
+            'pipe:1',
+        ]
+        print(" ".join(ffmpeg))
+        process =  await asyncio.subprocess.create_subprocess_exec(
+            *ffmpeg,
+            stdout=asyncio.subprocess.PIPE,
+            )
+        print("FFMPEG STARTED")
+        sendTask = asyncio.create_task(send(websocket,process))
         frames = Queue()
         audio = asyncio.Queue()
         print("sent metadata")
         recvTask = asyncio.create_task(recv(frames,audio,websocket))
-        # displayTask = threading.Thread(target = Display,args=(frames,))
-        audioTask = asyncio.create_task(playAudio(audio,))
-        # displayTask.start()
-        await asyncio.gather(sendTask, recvTask,)
-        # displayTask.join()
+        while frames.qsize()<17:
+            await asyncio.sleep(1/30)
+        audioTask = asyncio.create_task(playAudio(audio,frames,))
+        await asyncio.gather(
+sendTask, 
+recvTask,
+            )
+        await audioTask
         
+
+uvloop.install()
 asyncio.run(echo())
