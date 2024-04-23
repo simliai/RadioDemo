a73135a1657fb013bbec826a8112bf15a488e6c0
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2024-04-23 11:27:41 +0200

    Track webrtc renderer

---
+++ WebRTCRenderer/CustomMediaStreams.py
@@ -0,0 +1,326 @@
+import argparse
+import asyncio
+from hmac import new
+import json
+import logging
+import os
+from queue import Queue
+from re import A
+import ssl
+import time
+from typing import Optional
+import uuid
+import resampy
+import numpy as np
+import cv2
+from aiohttp import web
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription, AudioStreamTrack, VideoStreamTrack
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
+from aiortc.mediastreams import MediaStreamError
+from av import VideoFrame # type: ignore
+from av import AudioFrame # type: ignore
+import librosa
+import websockets
+import fractions
+
+class WSVideoTrack(MediaStreamTrack):
+    kind = "video"
+    def __init__(self):
+        super().__init__()
+        self.frameQueue:asyncio.Queue[bytes] = asyncio.Queue()
+        print("GNIERGNIRGN")
+        self.currentTime = 0
+        self.start=  time.time()
+    async def recv(self):
+        # print("recv called")
+        try:
+            self.start = time.time()
+            frame = await self.frameQueue.get()
+            if frame is None:
+                raise MediaStreamError()
+            frame = np.frombuffer(frame[12:], dtype=np.uint8)
+            frame = cv2.imdecode(frame, flags=1)
+            
+            new_frame = VideoFrame.from_ndarray(frame, format="bgr24")
+            new_frame.pts = self.currentTime
+            self.currentTime += int(1/30 * 90000)
+            new_frame.time_base = fractions.Fraction(1, 90000)
+            sleepTime = 1/30-time.time()+self.start
+            if sleepTime > 0:
+                await asyncio.sleep(sleepTime)
+            #     print("Video Sleeping", sleepTime)
+            # else:
+            #     print("Video NOT Sleeping", sleepTime)
+            return new_frame
+        except Exception as e:
+            print("EOGNEOGNOIEGNIOGENG",e)
+            import traceback
+            traceback.print_exc()
+
+class WSAudioTrack(MediaStreamTrack):
+    kind = "audio"
+    def __init__(self):
+        super().__init__()
+        self.frameQueue:asyncio.Queue[bytes] = asyncio.Queue()
+        self.timeStamp = 0
+        self.timeBase = fractions.Fraction(1,16000)
+        self.sampleRate = 16000
+        print("GNIRNGIn")
+        self.start = time.time()
+        self.first = True
+        self.dump = open("dump.pcm", "wb")
+
+    async def recv(self):
+        try:
+            s = time.time()
+            audio = await self.frameQueue.get()
+            if audio is None:
+                raise MediaStreamError()
+            # print(len(audio))
+            audio = np.frombuffer(audio, dtype=np.int16)
+            self.start = time.time()
+            if self.first:
+                print("Time till audio is received", time.time()-self.start)
+                print("Time waiting for first chunk", time.time()-s)
+                self.first = False
+            audio = audio.reshape(1,-1)
+            frame = AudioFrame.from_ndarray(audio, format="s16", layout="mono")
+            frame.sample_rate = 16000
+            frame.pts = self.timeStamp +1
+            self.timeStamp += audio.shape[1]
+            # print(audio.shape)
+            frame.sample_rate = self.sampleRate
+            frame.time_base = self.timeBase
+            self.dump.write(audio.tobytes())
+            sleepTime = 1/30-time.time()+self.start
+            if sleepTime > 0:
+                await asyncio.sleep(sleepTime)
+                print("Audio Sleeping", sleepTime)
+            # else:
+            #     # print("Audio NOT Sleeping", sleepTime)
+            return frame
+            
+        except Exception as e:
+            print("EOGNEOGNOIEGNIOGENG",e)
+
+    def __del__(self):
+        self.dump.close()
+
+class WebSocketReceiver(MediaStreamTrack):
+    def __init__(self, url:str, metadata:str):
+        self.metadata = metadata
+        self.url = url
+        self.Run = True
+        self.VideoStream = WSVideoTrack()
+        self.AudioStream = WSAudioTrack()
+        self.AudioStreamIn: Optional[AudioStreamTrack] = None
+        self.sentStamp = 0
+        
+    async def Config(self):
+        self.ws:websockets.WebSocketClientProtocol = await websockets.connect(self.url)
+        await self.ws.send(self.metadata)
+        self.first=  True
+        return self.ws
+    
+    async def populateQueue(self):
+        count = 0
+        reportLatencyCount = 10
+        while self.Run:
+            data = await self.ws.recv()
+            if data == "DONE":
+                print("DONE")
+                await self.VideoStream.frameQueue.put(None) # type: ignore
+                await self.AudioStream.frameQueue.put(None) # type: ignore
+                return
+            if data is not None and isinstance(data, bytes):
+                try:
+                    f = data[9 : 9 + int.from_bytes(data[5:9], "little")]
+                    await self.VideoStream.frameQueue.put(f)
+                    a = data[18 + int.from_bytes(data[5:9], "little") :]
+                    await self.AudioStream.frameQueue.put(a)
+                    if count < reportLatencyCount:
+                        # print("Audio Queue Size:", self.AudioStream.frameQueue.qsize())
+                        print("Latency:", time.time()-self.sentStamp)
+                        count += 1
+                except Exception as e:
+                    print("ROIGNGNRIGR",e)
+        
+    async def recv(self):
+        if not self.Run:
+            raise MediaStreamError()
+        internalBuffer = []
+        if self.AudioStreamIn is None:
+            print("Input Stream Not Configured")
+            return 
+        if not hasattr(self,'dumper'):
+            self.dumper = open("dumper.pcm", "wb")
+        # while self.Run:
+        if self.ws.open:
+            if self.first:
+                self.sentStamp = time.time()
+            try:
+                print("Waiting for Audio Stream")
+                data = await self.AudioStreamIn.recv()
+                print("RECEVIED")
+            except Exception as e:
+                self.dumper.close()
+                raise Exception("Error in Audio Stream", e)
+                # break
+                return
+            if self.first:
+                print("Time till audio is received", time.time()-self.sentStamp)
+            ogSampleRate:int = data.sample_rate
+            print(ogSampleRate)
+            datanp:np.ndarray = data.to_ndarray().astype(np.float32)
+            datanp = datanp.flatten()
+            try:
+                datanp = resampy.resample(datanp, sr_orig= ogSampleRate, sr_new= 16000)
+            except Exception as e:
+                print("Error in resampling", e)
+                raise MediaStreamError()
+            datanp = (datanp).astype(np.int16)
+            databytes = datanp[::2].tobytes()
+            if self.first:
+                print("Time till audio is processed", time.time()-self.sentStamp)
+                self.first = False
+            # print(len(databytes))
+            internalBuffer.append(databytes)
+            # if len(internalBuffer) < 10:
+            #     continue
+            self.dumper.write(b''.join(internalBuffer))
+            await self.ws.send(b''.join(internalBuffer))
+            internalBuffer = []
+            return data
+        else:
+            print("Connection is closed")
+        # self.dumper.close()
+
+    def start(self):
+        # self.ReceiveTask = asyncio.create_task(self.recv())
+        self.SendTask = asyncio.create_task(self.populateQueue())
+
+    async def GetStreams(self):
+        return self.VideoStream, self.AudioStream
+    
+    async def Exit(self):
+        self.Run = False
+        await self.ws.send(b"DONE")
+        await self.ws.close()
+        # await self.ReceiveTask
+        await self.SendTask
+
+class EchoTrack(MediaStreamTrack):
+    """
+    A media stream track that reflects the same media frame it receives.
+    """
+
+    def __init__(self, track):
+        super().__init__()  # don't forget this!
+        self.track = track
+        self.kind = track.kind
+
+    async def recv(self):
+        s = time.time()
+        frame = await self.track.recv()
+        print("Latency:", time.time()-s)
+        if self.track.kind == "audio":
+            print(frame.sample_rate)
+        return frame
+
+
+class WSRadio:
+    
+    async def Config(self,metadata:dict):
+        self.Run = True
+        self.url = "https://radio.talksport.com/stream"
+        self.ffmpeg = [
+            "ffmpeg",
+            '-nostdin',
+            '-v',
+            'error',
+            "-i",
+            self.url,
+            "-f",
+            's16le',
+            "-acodec",
+            "pcm_s16le",
+            "-ar", 
+            "16000",
+            "-ac",
+            "1",
+            'pipe:1',
+        ]
+        self.process = await asyncio.subprocess.create_subprocess_exec(
+            *self.ffmpeg,
+            stdout=asyncio.subprocess.PIPE,
+            )
+        self.websocket = await websockets.connect("ws://34.91.9.107:8892/LipsyncStream")
+        self.sendTask = asyncio.create_task(self.send())
+        self.audioTrack = WSAudioTrack()
+        self.videoTrack = WSVideoTrack()
+        self.frames = self.videoTrack.frameQueue
+        self.audio = self.audioTrack.frameQueue
+        print("sent metadata")
+        self.recvTask = asyncio.create_task(self.recv())
+        await self.websocket.send(json.dumps(metadata))
+        return self.websocket
+    
+    async def send(self):
+        print("SENDING")
+        while self.Run:
+            if self.process.stdout is None:
+                print("NO STDOUT")
+                break
+            data = await self.process.stdout.read(4096)
+            await self.websocket.send(data)
+        self.process.kill()
+        await self.process.wait()
+    
+    async def recv(self,):
+        try:
+            while self.Run:
+                # print("RECEIVING")
+                frame = await self.websocket.recv()
+                # print(len(frame))
+                if frame == "DONE":
+                    print("DONE")
+                    await self.frames.put(False)
+                    await self.audio.put(None)
+                    return
+                if isinstance(frame,bytes) and  frame.startswith(b"ChunkStart"):
+                    print("CHUNK START" , int.from_bytes(frame[10:14], 'little'))
+                    await self.frames.put(True)
+                    continue
+                # frame = cv2.imdecode(np.frombuffer(frame, dtype=np.uint8), flags=1)
+                if frame is not None and isinstance(frame, bytes) and len(frame)>0:
+                    # print("RECEIVED", len(frame))
+                    try:
+                        f = frame[9:9+int.from_bytes(frame[5:9], 'little')]
+                        # f = np.frombuffer(f[12:], dtype=np.uint8)
+                        # f = cv2.imdecode(f, flags=1)
+                        await self.frames.put(f)
+                        a = frame[18+int.from_bytes(frame[5:9], 'little'):]
+                        await self.audio.put(a)
+                        c = time.time()
+                        s = c
+                        # first = False
+                        # print("received frame")
+                    except Exception as e:
+                        print(e)
+        except websockets.exceptions.ConnectionClosed as e:
+            print("DISCONNECTED", e)
+
+        except Exception as e:
+            print("ENOF", e)
+            pass
+        await self.frames.put(False)
+    
+    async def GetStreams(self):
+        return self.videoTrack, self.audioTrack
+    
+    async def Exit(self):
+        self.Run = False
+        await self.websocket.send(b"DONE")
+        await self.websocket.close()
+        # await self.ReceiveTask
+        await self.sendTask
---
+++ WebRTCRenderer/server.py
@@ -0,0 +1,173 @@
+import argparse
+import asyncio
+from hmac import new
+import json
+import logging
+import os
+import ssl
+import uuid
+
+# import uvloop
+import numpy as np
+import cv2
+from aiohttp import web
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from CustomMediaStreams import WSRadio, WebSocketReceiver, EchoTrack
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
+import fractions
+
+ROOT = os.path.dirname(__file__)
+# uvloop.install()
+logger = logging.getLogger("pc")
+pcs = set()
+relay = MediaRelay()
+bh = MediaBlackhole()
+
+
+async def index(request):
+    content = open(os.path.join(ROOT, "index.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+
+async def javascript(request):
+    content = open(os.path.join(ROOT, "client.js"), "r").read()
+    return web.Response(content_type="application/javascript", text=content)
+
+
+async def offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+    print(offer)
+    pc = RTCPeerConnection()
+    pc_id = "PeerConnection(%s)" % uuid.uuid4()
+    pcs.add(pc)
+
+    def log_info(msg, *args):
+        logger.info(pc_id + " " + msg, *args)
+
+    log_info("Created for %s", request.remote)
+
+    # prepare local media
+    player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
+    if args.record_to:
+        recorder = MediaRecorder(args.record_to)
+    else:
+        recorder = MediaBlackhole()
+
+    @pc.on("datachannel")
+    def on_datachannel(channel):
+        @channel.on("message")
+        def on_message(message):
+            if isinstance(message, str) and message.startswith("ping"):
+                channel.send("pong" + message[4:])
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        log_info("Connection state is %s", pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    # saveFile = open("saveFile.pcm", "wb")
+    metadata = {
+        "video_reference_url": "https://storage.googleapis.com/charactervideos/11c30c18-86c3-424e-bb29-9c6d1fd6003b/11c30c18-86c3-424e-bb29-9c6d1fd6003b.mp4",
+        "face_det_results": "https://storage.googleapis.com/charactervideos/11c30c18-86c3-424e-bb29-9c6d1fd6003b/11c30c18-86c3-424e-bb29-9c6d1fd6003b.pkl",
+        "isSuperResolution": True,
+        "isJPG": True,
+        "syncAudio": True,
+    }
+    a = WSRadio()
+    await a.Config(metadata)
+    vt, at = await a.GetStreams()
+
+    @pc.on("track")
+    def on_track(track):
+        log_info("Track %s received", track.kind)
+
+        if track.kind == "audio":
+            pc.addTrack(
+                at
+            )
+
+            bh.addTrack(track)
+
+        elif track.kind == "video":
+            pc.addTrack(
+                vt
+            )
+            bh.addTrack(track)
+            if args.record_to:
+                recorder.addTrack(relay.subscribe(track))
+
+        @track.on("ended")
+        async def on_ended():
+            log_info("Track %s ended", track.kind)
+            await recorder.stop()
+            await pc.close()
+            await bh.stop()
+            await a.Exit()
+
+    # handle offer
+    await pc.setRemoteDescription(offer)
+    await recorder.start()
+    await bh.start()
+    # send answer
+    answer = await pc.createAnswer()
+    if answer is None:
+        return web.Response(
+            content_type="application/json",
+            text=json.dumps({"error": "Can't answer now"}),
+        )
+
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
+
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="WebRTC audio / video / data-channels demo"
+    )
+    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    parser.add_argument(
+        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+    )
+    parser.add_argument(
+        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+    )
+    parser.add_argument("--record-to", help="Write received media to a file.")
+    parser.add_argument("--verbose", "-v", action="count")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None
+
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_get("/", index)
+    app.router.add_get("/client.js", javascript)
+    app.router.add_post("/offer", offer)
+    web.run_app(
+        app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
+    )
